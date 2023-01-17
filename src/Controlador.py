d8c764c753801a093135b879c3e624a76c7e88d0
david <dhm1002@alu.ubu.es>
2023-01-17 23:48:03 +0100

    #9 Botón obras de teatro en menú principal

--- src/Controlador.py
+++ src/Controlador.py
@@ -96,6 +96,20 @@ def index():
             return render_template('index.html', error = error)
     return render_template('index.html')
 
+@app.route('/Sel-Corpus/', methods=["GET","POST"])
+def obraTeatro():
+    error = ''
+    if request.method == "POST":
+        m = mod.Modelo()
+        if('usuario' not in session or session['usuario'] not in tbd.getSesiones().keys()):
+            session['usuario'] = tbd.addSesion(m)
+            dirName = os.path.join(app.config['UPLOAD_FOLDER'], str(session['usuario']))
+            if(not os.path.exists(dirName)):
+                os.makedirs(dirName)
+        
+    return render_template('corpus.html')
+
+
 @app.route('/Acerca', methods=["GET","POST"])
 def about():
     return render_template('about.html')
