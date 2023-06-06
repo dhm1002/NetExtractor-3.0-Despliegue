0167201f152a9d590b0e5dae0cbff811be01c077
david <dhm1002@alu.ubu.es>
2023-06-06 20:29:35 +0200

    despliegue

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gettingstarted.wsgi --log-file -
+web: python main.py && gunicorn app:'create_app()'
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from tst import testUnitarios
+#from tst import testUnitarios
 from src import Controlador
 import unittest
+import os
 
 #runner = unittest.TextTestRunner()
-#result = runner.run(unittest.makeSuite(testUnitarios.testUnitarios))
+#result = runner.run(unittest.makeSuite(testUnitarios.TestUnitarios))
 
-Controlador.app.run(threaded=True, debug=True)
-
+port = int(os.environ.get('PORT', 5000))
+Controlador.app.run(threaded=True, host='0.0.0.0', port=port)
